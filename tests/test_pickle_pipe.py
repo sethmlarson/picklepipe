d4cf6201e1bf16c94fbb1c8b69442c074456ca16
lars4839 <lars4839@umn.edu>
2016-12-28 20:31:11 -0600

    Don't allocate 4GB

--- tests/test_pickle_pipe.py
+++ tests/test_pickle_pipe.py
@@ -12,11 +12,3 @@ class PickleTestCase(_base_pipe_testcase.BasePipeTestCase):
         rd, wr = self.make_pipe_pair()
         self.assertEqual(rd.protocol, pickle.HIGHEST_PROTOCOL)
         self.assertEqual(wr.protocol, pickle.HIGHEST_PROTOCOL)
-
-    @unittest.skipIf('TRAVIS' in os.environ, 'Skip this test on Travis CI.')
-    def test_pipe_send_large_object(self):
-        _, wr = self.make_pipe_pair()
-        try:
-            self.assertRaises(picklepipe.PipeObjectTooLargeError, wr.send_object, b'x' * (0xFFFFFFFF + 1))
-        except OverflowError:
-            self.skipTest('On some platforms using a value >0xFFFFFFFF actually overflows integers.')
